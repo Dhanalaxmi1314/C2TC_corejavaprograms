c989a7b43442eaabc123b558a5ab5aaa193c9b03
arull <arull@DHANALAXMI>
2025-08-19 14:10:50 +0530

    Single Inheritance

---
+++ javasample/src/singleinheritance_example/SlInheritanceDemo.java
@@ -0,0 +1,12 @@
+package singleinheritance_example;
+
+public class SlInheritanceDemo {
+	
+	public static void main(String[]args){
+		Trainer t=new Trainer(112,"Dhanalaxmi","Puducherry",56,"lead","Puducherry");
+		System.out.println(t);
+		Person p=new Person(223,"Dhana","chennai");
+		System.out.println(p);
+	}
+
+}
