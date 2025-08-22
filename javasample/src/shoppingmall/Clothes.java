52f0647b9b4fdd87cfdec4be819008c96b8aa561
arull <arull@DHANALAXMI>
2025-08-22 10:18:45 +0530

    Shopping mall interface

---
+++ javasample/src/shoppingmall/Clothes.java
@@ -0,0 +1,7 @@
+package shoppingmall;
+
+public class Clothes implements Shopping{
+             public void purchase() {
+            	 System.out.println("to purchase dress in trends");
+             }
+}
---
+++ javasample/src/shoppingmall/ShoppingMallDemo.java
@@ -0,0 +1,15 @@
+package shoppingmall;
+
+public class ShoppingMallDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shopping s;
+		s=new Clothes();
+		s.purchase();
+		
+		
+
+	}
+
+}
